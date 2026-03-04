9536fa0f96b177939574666c4f4d690ce68c8e2b
Aoife Bee <aoifebeeof@gmail.com>
2026-03-04 14:23:10 -0500

    updated thumbnails to only accept JPG or PNG images

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -40,6 +40,9 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   if (!mediaType) {
     throw new BadRequestError("Missing Content-Type for thumbnail");
   }
+  if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("Unsupported media type. Thumbnail must be a JPEG or PNG.");
+  }
 
   const fileData = await file.arrayBuffer();
 
